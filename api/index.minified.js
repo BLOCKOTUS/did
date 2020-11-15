54be69a22589a75ba0af1d620291f13a5e7e7293
Daniel Febrero <febrero@me.com>
2020-11-15 12:28:09 +0100

    BLOCKOTUS/did#6 DID url constructor

--- api/index.minified.js
+++ api/index.minified.js
@@ -1,12 +1,30 @@
 "use strict";
 
+require("core-js/modules/es.symbol");
+
+require("core-js/modules/es.symbol.description");
+
+require("core-js/modules/es.symbol.iterator");
+
 require("core-js/modules/es.array.concat");
 
+require("core-js/modules/es.array.includes");
+
+require("core-js/modules/es.array.iterator");
+
 require("core-js/modules/es.array.join");
+
+require("core-js/modules/es.array.reduce");
+
+require("core-js/modules/es.object.keys");
 
 require("core-js/modules/es.object.to-string");
 
 require("core-js/modules/es.promise");
+
+require("core-js/modules/es.string.iterator");
+
+require("core-js/modules/web.dom-collections.iterator");
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -23,7 +41,51 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 var WALLET_PATH = _path["default"].join(__dirname, '..', '..', '..', 'wallet');
+
+var objectToQueryString = function objectToQueryString(initialObj) {
+  var reducer = function reducer(obj) {
+    var parentPrefix = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+    return function (prev, key) {
+      var val = obj[key];
+      key = encodeURIComponent(key);
+      var prefix = parentPrefix ? "".concat(parentPrefix, "[").concat(key, "]") : key;
+
+      if (val == null || typeof val === 'function') {
+        prev.push("".concat(prefix, "="));
+        return prev;
+      }
+
+      if (['number', 'boolean', 'string'].includes(_typeof(val))) {
+        prev.push("".concat(prefix, "=").concat(encodeURIComponent(val)));
+        return prev;
+      }
+
+      prev.push(Object.keys(val).reduce(reducer(val, prefix), []).join('&'));
+      return prev;
+    };
+  };
+
+  return Object.keys(initialObj).reduce(reducer(initialObj), []).join('&');
+};
+/**
+ * DID url constructor helpers.
+ */
+
+
+var makePath = function makePath(urlPath) {
+  return urlPath && urlPath.substr(0, 1) === '/' ? urlPath : '';
+};
+
+var makeQuery = function makeQuery(query) {
+  return query ? "?".concat(objectToQueryString(query)) : '';
+};
+
+var makeFragment = function makeFragment(fragment) {
+  return fragment ? "#".concat(fragment) : '';
+};
 /**
  * Construct a DID url based on arguments.
  * The DID url will be consumed by Chaincode Contracts.
@@ -35,10 +97,10 @@ var WALLET_PATH = _path["default"].join(__dirname, '..', '..', '..', 'wallet');
 var makeUrl = function makeUrl(_ref) {
   var methodName = _ref.methodName,
       methodSpecificId = _ref.methodSpecificId,
-      path = _ref.path,
+      urlPath = _ref.urlPath,
       query = _ref.query,
       fragment = _ref.fragment;
-  return "did:".concat(methodName, ":").concat(methodSpecificId);
+  return "did:".concat(methodName, ":").concat(methodSpecificId).concat(makePath(urlPath)).concat(makeQuery(query)).concat(makeFragment(fragment));
 };
 /**
  * Make a request to the network, using a DID url.
@@ -48,12 +110,12 @@ var makeUrl = function makeUrl(_ref) {
 
 var request = /*#__PURE__*/function () {
   var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(_ref2) {
-    var url, methodName, methodSpecificId, path, query, fragment;
+    var url, methodName, methodSpecificId, urlPath, query, fragment;
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            url = _ref2.url, methodName = _ref2.methodName, methodSpecificId = _ref2.methodSpecificId, path = _ref2.path, query = _ref2.query, fragment = _ref2.fragment;
+            url = _ref2.url, methodName = _ref2.methodName, methodSpecificId = _ref2.methodSpecificId, urlPath = _ref2.urlPath, query = _ref2.query, fragment = _ref2.fragment;
 
             if (!(!url && (!methodName || !methodSpecificId))) {
               _context.next = 3;
@@ -66,11 +128,15 @@ var request = /*#__PURE__*/function () {
             if (!url) url = makeUrl({
               methodName: methodName,
               methodSpecificId: methodSpecificId,
+              urlPath: urlPath,
               query: query,
               fragment: fragment
             });
+            console.log({
+              url: url
+            });
 
-          case 4:
+          case 5:
           case "end":
             return _context.stop();
         }
--- api/index.ts
+++ api/index.ts
@@ -6,6 +6,40 @@ import { getContractAndGateway } from '../../helper/api/index.minified.js';
 const WALLET_PATH = path.join(__dirname, '..', '..', '..', 'wallet');
 
 /**
+ * Transform a javascript object to a Query String
+ * https://gist.github.com/tjmehta/9204891#gistcomment-3527084
+ */
+const objectToQueryString = (initialObj: Record<string, any>): string => {
+  const reducer = (obj, parentPrefix = null) => (prev, key) => {
+    const val = obj[key];
+    key = encodeURIComponent(key);
+    const prefix = parentPrefix ? `${parentPrefix}[${key}]` : key;
+
+    if (val == null || typeof val === 'function') {
+      prev.push(`${prefix}=`);
+      return prev;
+    }
+
+    if (['number', 'boolean', 'string'].includes(typeof val)) {
+      prev.push(`${prefix}=${encodeURIComponent(val)}`);
+      return prev;
+    }
+
+    prev.push(Object.keys(val).reduce(reducer(val, prefix), []).join('&'));
+    return prev;
+  };
+
+  return Object.keys(initialObj).reduce(reducer(initialObj), []).join('&');
+};
+
+/**
+ * DID url constructor helpers.
+ */
+const makePath = (urlPath: string) => urlPath && urlPath.substr(0, 1) === '/' ? urlPath : '';
+const makeQuery = (query: Record<string, any>) => query ? `?${objectToQueryString(query)}` : '';
+const makeFragment = (fragment: string) => fragment ? `#${fragment}` : '';
+
+/**
  * Construct a DID url based on arguments.
  * The DID url will be consumed by Chaincode Contracts.
  * did-url = did path-abempty [ "?" query ] [ "#" fragment ]
@@ -14,16 +48,16 @@ const WALLET_PATH = path.join(__dirname, '..', '..', '..', 'wallet');
 const makeUrl = ({
   methodName,
   methodSpecificId,
-  path,
+  urlPath,
   query,
   fragment,
 }: {
   methodName: string,
   methodSpecificId: string,
-  path?: string,
-  query?: string,
+  urlPath?: string,
+  query?: Record<string, any>,
   fragment?: string,
-}): string => `did:${methodName}:${methodSpecificId}`;
+}): string => `did:${methodName}:${methodSpecificId}${makePath(urlPath)}${makeQuery(query)}${makeFragment(fragment)}`;
 
 /**
  * Make a request to the network, using a DID url.
@@ -33,17 +67,18 @@ export const request = async ({
   url,
   methodName,
   methodSpecificId,
-  path,
+  urlPath,
   query,
   fragment,
 }: {
   url?: string,
   methodName?: string,
   methodSpecificId?: string,
-  path?: string,
-  query?: string,
+  urlPath?: string,
+  query?: Record<string, any>,
   fragment?: string,
 }): Promise<any> => {
   if (!url && (!methodName || !methodSpecificId)) throw new Error('Url or methodName and methodSpecificId are missing.');
-  if (!url) url = makeUrl({ methodName, methodSpecificId, query, fragment });
+  if (!url) url = makeUrl({ methodName, methodSpecificId, urlPath, query, fragment });
+  console.log({url});
 };
