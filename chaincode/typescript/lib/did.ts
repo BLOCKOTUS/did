3f51d6bdfcbde8f9e7e42dca9813fa215bf706b5
Daniel Febrero <febrero@me.com>
2020-11-17 13:46:09 +0100

    BLOCKOTUS/did#3 change reFragment

--- chaincode/typescript/lib/did.ts
+++ chaincode/typescript/lib/did.ts
@@ -35,7 +35,7 @@ export class Did extends Contract {
     }
 
     private parseDidUrl = (ur: DIDUrl): ParsedDIDUrl => {
-        let reFragment = /#[\w?\/]*+$/g;
+        let reFragment = /#[\w?\/#]*/g;
         let reQuery = /\?.*/g; // remove fragment match before matching query
         let rePath = /\/(.)*/g; // remove query match before matching path
     }
